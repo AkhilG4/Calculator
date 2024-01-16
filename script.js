18d7c867d289f565f137fc5e3814985b9c096daf
Akhil <88569103+RapidCow1@users.noreply.github.com>
2024-01-16 22:46:19 +0800

    Link operate function with previous functions

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function divide(a,b) {
 let numOne = 0;
 let numTwo = 0;
 let operator = '';
+
+function operate(numOne, numTwo, operator) {
+    switch (operator) {
+        case '+':
+            return add(numOne,numTwo);
+            break;
+        case '-':
+            return subtract(numOne,numTwo);
+            break;
+        case '*':
+            return multiply(numOne,numTwo);
+        case '/':
+            return divide(numOne, numTwo);
+            break;
+        default:
+            alert("ERROR")
+    }
+}
