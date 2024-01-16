8057df0ce37a7bc25139f8e2da6a45dbe0410f21
Akhil <88569103+RapidCow1@users.noreply.github.com>
2024-01-16 22:50:57 +0800

    Remove unnecessary breaks after return calls

--- script.js
+++ script.js
@@ -22,15 +22,12 @@ function operate(numOne, numTwo, operator) {
     switch (operator) {
         case '+':
             return add(numOne,numTwo);
-            break;
         case '-':
             return subtract(numOne,numTwo);
-            break;
         case '*':
             return multiply(numOne,numTwo);
         case '/':
             return divide(numOne, numTwo);
-            break;
         default:
             alert("ERROR")
     }
