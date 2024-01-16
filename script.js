45924315c1998fa45e4997596f37f9586a983205
Akhil <88569103+RapidCow1@users.noreply.github.com>
2024-01-16 13:26:55 +0800

    Create math operator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+let numOne = 0;
+let numTwo = 0;
+let operator = '';
